45ec741755c9ef2ebf8682adb1b9948b7f9732ab
FriedrichNaum <83044596+FriedrichNaum@users.noreply.github.com>
2024-08-26 10:46:46 -0300

    Botão de Cancelar
    Apenas um pequeno detalhe em fazer o botão desaparecer

--- src/resources/views/solicitacoes/index.blade.php
+++ src/resources/views/solicitacoes/index.blade.php
@@ -15,9 +15,24 @@
             <form action="{{ route('solicitacoes.destroy', $solicitacao->id) }}" method="POST" style="display:inline;">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="btn btn-danger">Cancelar</button>
+                <button id="cancelarSolicitacao" type="submit" class="btn btn-danger">Cancelar</button>
             </form>
         </li>
     @endforeach
 </ul>
+
+<script>
+        // Função que esconde o botão após determinado tempo
+        function esconderBotao() {
+            const botao = document.getElementById('cancelarSolicitacao');
+
+            // Define o tempo para esconder o botão
+            setTimeout(() => {
+                botao.style.display = 'none';
+            }, 30000); // 30000 milissegundos = 30 segundos
+        }
+
+        // Esconde o botão quando a página carrega
+        window.onload = esconderBotao;
+    </script>
 </x-layout>
