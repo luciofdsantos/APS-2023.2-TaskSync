8f7b69b4da5da02dadfbd8661d696d6b4eb866e0
LucasFreitaslpf1 <Lucas9d@gmail.com>
2024-09-03 15:56:35 -0300

    refactor: tira javscript de area de serviço edit

--- src/resources/views/area-de-servico/edit.blade.php
+++ src/resources/views/area-de-servico/edit.blade.php
@@ -1,17 +1,20 @@
 <!doctype html>
 <html lang="pt">
+
 <head>
-    <x-header-layout/>
+    <x-header-layout />
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
 </head>
+
 <body>
     <div class="content-container">
         <main class="main-cntt">
             <div class="content-box">
                 <a class="btn btn-secondary" href="{{ route('area-de-servico.index') }}">Voltar</a>
-                <form method="post" action="{{ route('area-de-servico.update', ['area_de_servico' => $area_de_servico]) }}"
+                <form method="post"
+                    action="{{ route('area-de-servico.update', ['area_de_servico' => $area_de_servico]) }}"
                     class="form-control">
                     @csrf
                     @method('put')
@@ -27,7 +30,8 @@
                     <label for="gerente_id">Gerente responsável</label>
                     <select class="form-control" name="gerente_id" value="{{ $area_de_servico->gerente_id }}">
                         @foreach ($gerentes as $gerente)
-                            <option value="{{ $gerente['id'] }}">{{ $gerente->user->name . ' - ' . $gerente->cpf }}</option>
+                            <option value="{{ $gerente['id'] }}">{{ $gerente->user->name . ' - ' . $gerente->cpf }}
+                            </option>
                         @endforeach
                     </select>
                     @error('gerente_id')
@@ -52,17 +56,9 @@
             </div>
         </main>
     </div>
-    <x-item-layout/>
-<body>
+    <x-item-layout />
 
-<script>
-    const toSelect = document.getElementById('selected');
-    const optionElements = toSelect.getElementsByTagName('option');
-    const optionsArray = Array.from(toSelect.options);
+    <body>
 
-    optionsArray.forEach(element => {
-        element.selected = true
-    });
 
-</script>
 </html>
