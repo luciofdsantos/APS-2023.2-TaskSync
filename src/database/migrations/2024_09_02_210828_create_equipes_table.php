85b31d7b5e32139c0af08778366b35c572e390d0
LucasFreitaslpf1 <Lucas9d@gmail.com>
2024-09-02 23:15:54 -0300

    migration de equipe

---
+++ src/database/migrations/2024_09_02_210828_create_equipes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipe', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 255)->nullable(false);
+            $table->text('descricao')->nullable(false);
+            $table->timestamps();
+        });
+
+
+        Schema::create('funcionario_equipe', function (Blueprint $table) {
+            $table->foreignId('funcionario_id')->references('id')->on('usuario')->cascadeOnDelete();
+            $table->foreignId('equipe_id')->references('id')->on('equipe')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipe');
+        Schema::dropIfExists('funcionario_equipe');
+    }
+};
