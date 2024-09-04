21c1d8dae60abc3ecc02152e5b2617ac2775b3e3
LucasFreitaslpf1 <Lucas9d@gmail.com>
2024-09-04 09:34:27 -0300

    tira bug da logo

--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -21,15 +21,18 @@
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
 
+
 </head>
+
 <body>
     <div class="flex-grow-1" style="padding: 15px;">
-        
+
         <!-- Header fixo no topo -->
-        <header class="bg-white text-dark p-4 rounded-3" style="height: 70px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); position: fixed; top: 0; left: 0; right: 0; z-index: 1000; margin: 15px;">
+        <header class="bg-white text-dark p-4 rounded-3"
+            style="height: 70px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); position: fixed; top: 0; left: 0; right: 0; z-index: 1000; margin: 15px;">
             <div class="container d-flex justify-content-between align-items-center">
-                <a href="{{route('dashboard')}}">
-                <img src="images/taskSync_logo.png" class="btnLogo" alt="alt text">
+                <a href="{{ route('dashboard') }}">
+                    <img src="{{ asset('images/taskSync_logo.png') }}" class="btnLogo" alt="alt text">
                 </a>
                 <form method="POST" action="{{ route('logout') }}" class="d-inline">
                     @csrf
@@ -39,8 +42,8 @@
                 </form>
             </div>
         </header>
-        
-        
+
+
 
         <!-- Espaçamento para garantir que o conteúdo não fique coberto pelo header -->
         <div style="height: 45px;"></div>
@@ -49,9 +52,10 @@
         <!--
         <main id="mainContent" class="p-4">
             <div class="bg-white shadow-lg rounded-3 p-4">
-                
+
             </div>
         </main>-->
-    </div> 
+    </div>
 </body>
+
 </html>
