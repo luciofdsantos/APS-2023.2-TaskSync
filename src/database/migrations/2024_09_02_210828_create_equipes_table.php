0cc24db1e87bb2a534df3817f2633ccf9ea26a87
LucasFreitaslpf1 <Lucas9d@gmail.com>
2024-09-17 19:09:05 -0300

    fix: corrige ordem no migrate de equipe

--- src/database/migrations/2024_09_02_210828_create_equipes_table.php
+++ src/database/migrations/2024_09_02_210828_create_equipes_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('equipe');
         Schema::dropIfExists('funcionario_equipe');
+        Schema::dropIfExists('equipe');
     }
 };
